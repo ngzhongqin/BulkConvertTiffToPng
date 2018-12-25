15012de27e9f412afafd05fab5fa47eb6fdb0a47
zhongqin <zhongqin@bandboo.co>
2018-12-25 13:46:31 +0800

    Identify filename

--- src/main/java/com/ngzhongqin/bulkConvertTiffToPng/Main.java
+++ src/main/java/com/ngzhongqin/bulkConvertTiffToPng/Main.java
@@ -6,12 +6,14 @@ import org.apache.log4j.Logger;
 
 public class Main {
     public final static Logger logger = Logger.getLogger(Main.class);
+    public static String fromFolder="/Users/zhongqin/IdeaProjects/BulkConvertTiffToPng";
+    public static String toFolder="/Users/zhongqin/target";
 
     public static void main(String[] args) throws Exception {
         BasicConfigurator.configure();
         logger.info("Started Main.main");
         DirectoryTreeWalker directoryTreeWalker = new DirectoryTreeWalker();
-        directoryTreeWalker.walkAndConvert("/Users/zhongqin/IdeaProjects/BulkConvertTiffToPng");
+        directoryTreeWalker.walkAndConvert();
 
     }
 }
--- src/main/java/com/ngzhongqin/bulkConvertTiffToPng/worker/DirectoryTreeWalker.java
+++ src/main/java/com/ngzhongqin/bulkConvertTiffToPng/worker/DirectoryTreeWalker.java
@@ -17,23 +17,57 @@ public class DirectoryTreeWalker {
     public DirectoryTreeWalker(){
 
     }
-    public void walkAndConvert(String folderPath){
-        try (Stream<Path> paths = Files.walk(Paths.get(folderPath))) {
+    public void walkAndConvert(){
+        try (Stream<Path> paths = Files.walk(Paths.get(Main.fromFolder))) {
             paths.forEach(
                     new Consumer<Path>() {
                           @Override
                           public void accept(Path path) {
-                            logger.info(path);
-                              try {
-                                  new TiffToPngWorker().convert("sample.tif","sample.png");
-                              } catch (IOException e) {
-                                  e.printStackTrace();
-                              }
+//                              logger.info("path:"+path);
+//                              logger.info("path.getRoot():"+path.getRoot());
+//                              logger.info("getRelativePath:"+getRelativePath(Main.fromFolder,path));
+//                              logger.info("path.getFileName():"+path.getFileName());
+                            convertAndStoreTif(path);
+
+
                           }
                       }
             );
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static String getRelativePath(String folderPath, Path path){
+        if(path==null){
+            return "";
+        }
+        String rPath = path.toString();
+        return rPath.replace(folderPath,"");
+    }
+
+    public static String getRelativePathWithoutFileName(Path path){
+        if(path==null){
+            return "";
+        }
+        String rPath=getRelativePath(Main.fromFolder,path);
+        return rPath.replace(path.getFileName().toString(),"");
+    }
+
+    public void convertAndStoreTif(Path path){
+        if(path.getFileName().toString().endsWith(".tif")){
+            logger.info("path:"+path);
+            logger.info("path.getRoot():"+path.getRoot());
+            logger.info("getRelativePath:"+getRelativePath(Main.fromFolder,path));
+            logger.info("path.getFileName():"+path.getFileName());
+            String newFilePath = Main.toFolder+getRelativePathWithoutFileName(path)+path.getFileName().toString().replace(".tif",".png");
+            logger.info("newFilePath:"+newFilePath);
+            try {
+                    new TiffToPngWorker().convert(path.toString(),newFilePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
         }
     }
 }
